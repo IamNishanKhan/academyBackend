fe119e5bf3a5a97059eb935300c81b3c2ef97ec4
Nishan Khan <iamnishankhan@gmail.com>
2025-02-15 02:03:49 +0600

    enrollments api updated

--- enrollments/urls.py
+++ enrollments/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from .views import EnrollmentViewSet
 
 router = DefaultRouter()
-router.register(r'enrollments', EnrollmentViewSet)
+router.register(r'enrollments', EnrollmentViewSet, basename="enrollment")
 
 urlpatterns = router.urls
